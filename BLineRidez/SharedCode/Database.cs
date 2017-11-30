594c52ad778c198fa8da0db421ea1e03f0f6cf9c
jakezenger <jakezenger@hotmail.com>
2017-11-29 16:30:49 -0800

    Added connection string and constructor for Database

--- BLineRidez/SharedCode/Database.cs
+++ BLineRidez/SharedCode/Database.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,17 @@ namespace BLineRidez.SharedCode
 {
     public class Database
     {
+        private string CONNECTION_STRING;
+
+        public Database()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "b-lineridez.database.windows.net";
+            builder.UserID = "jakezenger";
+            builder.Password = "orangeChicken17";
+            builder.InitialCatalog = "BLineRidezDB";
+
+            CONNECTION_STRING = builder.ConnectionString.ToString();
+        }
     }
 }
