a268465a690a7bd41ac6d250bfb15b4e79b9cb15
jakezenger <jakezenger@hotmail.com>
2017-11-30 18:43:07 -0800

    Added AddDriver() and AddCustomer() to Database

--- BLineRidez/SharedCode/Database.cs
+++ BLineRidez/SharedCode/Database.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using BLineRidez.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Data;
 
 namespace BLineRidez.SharedCode
 {
@@ -21,27 +23,92 @@ namespace BLineRidez.SharedCode
             CONNECTION_STRING = builder.ConnectionString.ToString();
         }
 
-        public int Execute(string sqlCommandString)
+        public bool AddCustomer(Customer customer, string password)
         {
-            using (var sqlConnection = new SqlConnection())
+            using (SqlConnection connection = new SqlConnection(CONNECTION_STRING))
             {
                 try
                 {
-                    sqlConnection.ConnectionString = CONNECTION_STRING;
-                    sqlConnection.Open();
+                    connection.Open();
 
-                    var command = new SqlCommand(sqlCommandString, sqlConnection);
+                    string cmdString = String.Format("SELECT dbo.fnCheckUserNameAvailability({0})", customer.Username);
+                    SqlCommand checkUsernameAvailabilityCmd = new SqlCommand(cmdString, connection);
 
-                    int result = command.ExecuteNonQuery();
+                    bool usernameIsAvailable = (bool)checkUsernameAvailabilityCmd.ExecuteScalar();
 
-                    sqlConnection.Close();
+                    if (usernameIsAvailable)
+                    {
+                        SqlCommand insertUserCmd = new SqlCommand("spInsertCustomer", connection);
+                        insertUserCmd.CommandType = CommandType.StoredProcedure;
+                        insertUserCmd.Parameters.Add("@UserName", SqlDbType.NVarChar).Value = customer.Username;
+                        insertUserCmd.Parameters.Add("@FirstName", SqlDbType.NVarChar).Value = customer.FirstName;
+                        insertUserCmd.Parameters.Add("@LastName", SqlDbType.NVarChar).Value = customer.LastName;
+                        insertUserCmd.Parameters.Add("@Password", SqlDbType.NVarChar).Value = password;
+                        insertUserCmd.Parameters.Add("@Email", SqlDbType.NVarChar).Value = customer.Email;
+                        insertUserCmd.Parameters.Add("@PhoneNum", SqlDbType.NVarChar).Value = customer.Phone;
 
-                    return result;
+                        insertUserCmd.ExecuteNonQuery();
+
+                        connection.Close();
+                        return true;
+                    }
+                    else
+                    {
+                        connection.Close();
+                        return false;
+                    }
                 }
-                catch (Exception exception)
+                catch (Exception e)
                 {
-                    Console.WriteLine(exception.ToString());
-                    return 0;
+                    Console.WriteLine(e.ToString());
+                    return false;
+                }
+            }
+        }
+
+        public bool AddDriver(Driver driver, Car car, string password)
+        {
+            using (SqlConnection connection = new SqlConnection(CONNECTION_STRING))
+            {
+                try
+                {
+                    connection.Open();
+
+                    string cmdString = String.Format("SELECT dbo.fnCheckUserNameAvailability({0})", driver.Username);
+                    SqlCommand checkUsernameAvailabilityCmd = new SqlCommand(cmdString, connection);
+
+                    bool usernameIsAvailable = (bool)checkUsernameAvailabilityCmd.ExecuteScalar();
+
+                    if (usernameIsAvailable)
+                    {
+                        SqlCommand insertUserCmd = new SqlCommand("spInsertDriver", connection);
+                        insertUserCmd.CommandType = CommandType.StoredProcedure;
+                        insertUserCmd.Parameters.Add("@UserName", SqlDbType.NVarChar).Value = driver.Username;
+                        insertUserCmd.Parameters.Add("@FirstName", SqlDbType.NVarChar).Value = driver.FirstName;
+                        insertUserCmd.Parameters.Add("@LastName", SqlDbType.NVarChar).Value = driver.LastName;
+                        insertUserCmd.Parameters.Add("@Password", SqlDbType.NVarChar).Value = password;
+                        insertUserCmd.Parameters.Add("@Email", SqlDbType.NVarChar).Value = driver.Email;
+                        insertUserCmd.Parameters.Add("@PhoneNum", SqlDbType.NVarChar).Value = driver.Phone;
+                        insertUserCmd.Parameters.Add("@CarMake", SqlDbType.NVarChar).Value = car.Make;
+                        insertUserCmd.Parameters.Add("@CarColor", SqlDbType.NVarChar).Value = car.Color;
+                        insertUserCmd.Parameters.Add("@CarModel", SqlDbType.NVarChar).Value = car.Model;
+                        insertUserCmd.Parameters.Add("@CarYear", SqlDbType.NVarChar).Value = car.Year;
+
+                        insertUserCmd.ExecuteNonQuery();
+
+                        connection.Close();
+                        return true;
+                    }
+                    else
+                    {
+                        connection.Close();
+                        return false;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                    return false;
                 }
             }
         }
